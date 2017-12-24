5c0c4d9715953c78869842d73f21a9f29f83e0b5
CarbonylGroup <carbonylgp@gmail.com>
2017-12-24 22:18:48 +0800

    face detection

--- src/main/java/com/example/chatbot/FacebookBotVerticle.java
+++ src/main/java/com/example/chatbot/FacebookBotVerticle.java
@@ -12,17 +12,30 @@ import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.client.WebClientOptions;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.opencv_objdetect;
+import org.bytedeco.javacpp.opencv_core;
 
+import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+
 public class FacebookBotVerticle extends AbstractVerticle {
+
+    private static final String classifierURL = "https://raw.github.com/Itseez/opencv/2.4.0/data/haarcascades/haarcascade_frontalface_alt.xml";
 
     private String VERIFY_TOKEN;
     private String ACCESS_TOKEN;
 
+    private opencv_objdetect.CascadeClassifier classifier;
+
     @Override
-    public void start() {
+    public void start() throws Exception {
 
         updateProperties();
 
@@ -34,6 +47,10 @@ public class FacebookBotVerticle extends AbstractVerticle {
         vertx.createHttpServer().requestHandler(router::accept)
                 .listen(
                         Integer.getInteger("http.port", 8080), System.getProperty("http.address", "0.0.0.0"));
+
+        classifier = new opencv_objdetect.CascadeClassifier(
+                opencv_core.cvLoad(Loader.extractResource(
+                        new URL(classifierURL), null, "classifier", ".xml").getAbsolutePath()));
     }
 
     public static void main(String[] args) {
@@ -64,21 +81,50 @@ public class FacebookBotVerticle extends AbstractVerticle {
 
         JsonArray entries = hook.getJsonArray("entry");
 
-        entries.getList().forEach( (Object e) -> {
+        entries.getList().forEach((Object e) -> {
 
             System.out.println(e.getClass());
 
-            Map entry = (Map) e ;
+            Map entry = (Map) e;
             ArrayList messagingList = (ArrayList) entry.get("messaging");
             System.out.println(messagingList.getClass());
             messagingList.forEach((Object m) -> {
 
-                Map messaging = (Map) m ;
+                Map messaging = (Map) m;
                 Map sender = (Map) messaging.get("sender");
                 messaging.put("recipient", sender);
                 messaging.remove("sender");
 
                 Map message = (Map) messaging.get("message");
+
+                final ArrayList attachments = (ArrayList) message.get("attachments");
+                if (attachments != null) {
+                    try {
+                        final URL imageUrl = new URL(((String) ((Map) ((Map) attachments.get(0)).get("payload")).get("url")));
+                        final File imageFile = File.createTempFile("attachment-", FilenameUtils.getName(imageUrl.getPath()));
+                        FileUtils.copyURLToFile(imageUrl, imageFile);
+
+                        opencv_core.RectVector rectVector = new opencv_core.RectVector();
+                        classifier.detectMultiScale(imread(imageFile.getAbsolutePath()), rectVector);
+
+                        final long faceNumber = rectVector.size();
+
+                        //Face Detected
+                        if (rectVector.size() >= 0) {
+                            message.put("text",
+                                    "I see " +
+                                    (faceNumber == 0 ? "no" : faceNumber) +
+                                    " face" +
+                                    (rectVector.size() > 1 ? "s" : "") +
+                                    ".");
+                        }
+
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+                message.remove("attachments");
                 message.remove("mid");
                 message.remove("seq");
                 messaging.put("message", message);
@@ -108,10 +154,10 @@ public class FacebookBotVerticle extends AbstractVerticle {
     }
 
     private void updateProperties() {
-        
-            VERIFY_TOKEN = System.getProperty("facebook.verify.token", "verify-token-default");
-            ACCESS_TOKEN = System.getProperty("facebook.access.token", "access-token-default");
-        
+
+        VERIFY_TOKEN = System.getProperty("facebook.verify.token", "verify-token-default");
+        ACCESS_TOKEN = System.getProperty("facebook.access.token", "access-token-default");
+
     }
 }
 
